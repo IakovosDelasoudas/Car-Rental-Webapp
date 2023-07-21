b8331f335981d00cc291ded0c5ec8e9a03c1c703
IakovosDelasoudas <i.delasoudas@acg.edu>
2023-07-21 20:31:44 +0300

    site admin DONE

--- CarRental/rentalapp/models.py
+++ CarRental/rentalapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    seats = models.IntegerField()
+    color = models.CharField(max_length=20)
+    price_per_day = models.DecimalField(max_digits=5, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.make} {self.model}"
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"Booking {self.pk} by {self.user.username}"
